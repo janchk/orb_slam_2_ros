2a5d65edf3274b54b66ed8872d351685a931027f
jakhremchik <jakhremchik@lanit-tercom.com>
2021-05-21 16:45:08 +0300

    Changed params for subscibers

--- ros/src/MonoNode.cc
+++ ros/src/MonoNode.cc
@@ -26,8 +26,8 @@ int main(int argc, char **argv)
 
 
 MonoNode::MonoNode (ORB_SLAM2::System::eSensor sensor, ros::NodeHandle &node_handle, image_transport::ImageTransport &image_transport) : Node (sensor, node_handle, image_transport) {
-  image_subscriber = image_transport.subscribe ("/camera/image_raw", 1, &MonoNode::ImageCallback, this);
-  camera_info_topic_ = "/camera/camera_info";
+  image_subscriber = image_transport.subscribe ("/camera/fisheye1/rect/image", 1, &MonoNode::ImageCallback, this);
+  camera_info_topic_ = "/camera/fisheye1/rect/camera_info";
 }
 
 
